099d20b11e9566f210a1121fc5c75e86c881f5c8
UserMcUser <UserMcUser@users.noreply.github.com>
2015-06-14 18:39:36 -0400

    Version 1.0 Commit

--- mvm.js
+++ mvm.js
@@ -13,27 +13,30 @@ var MainPlayerPacket = new Buffer(5);
 MainPlayerPacket.writeUIntBE(0xf93d808001,0,5);
 
 var temp = Buffer(0);
+var showInstead = 0;
 
 program
 .version(packagejson.version)
-.description('Delay packets')
 .option('-l, --listen [port]', 'TCP port to listen on', parseInt)
 .option('-f, --forward [port]', 'TCP port to forward to (normally 2010)', parseInt)
-.option('-h, --host [hostname]', 'IP or Hostname of real Artemis Server. Defaults to \'localhost\' if unspecified.')
-.option('-v, --verbose', 'Log connection events')
+.option('-v, --view [0-6]', 'What should the mainscreen show?\n                       0=Fore, 1=Port, 2=Starboard, 3=Aft, 4=Tactical,\n                       5=Long Range Sensors, and 6=Ship Status')
+.option('-h, --host [hostname]', 'IP or Hostname of the real Artemis Server.\n                       Defaults to \'localhost\' if unspecified.')
 .parse(process.argv);
 
-
 if (!program.listen) {
-  console.error('Need a listening port');
+  console.error('Please specify listening port.');
   program.help();
 }
 
 if (!program.forward) {
-  console.error('Need a forwarding port');
+  console.error('Please specify forwarding port (usually 2010)');
   program.help();
 }
 
+if (!program.view) {
+  console.error('Please specify the desired view.');
+  program.help();
+}
 
 function createConnection() {
   var conn;
@@ -66,19 +69,19 @@ var server = net.createServer(function(listen) {
     logSocketStatus('(forwarding) client connected.', listen.forward);
   });
   listen.forward.on('data', function(data) {
-
     //MVM Lie Goes here.
     temp = data.slice(20,25);
     //console.log(temp);
 
     if (temp.equals(MainPlayerPacket)){
-      var bufferIndex = unpackBitmap(data.slice(29,35), data);
+      var bufferIndex = unpackBitmap(data.slice(29,34), data);
+
       if (bufferIndex){
-        console.log('bitmap: ', data.slice(29,33));
-        console.log('Server Says the mainScreen is: ', data.readUInt8(bufferIndex));
-        var showInstead = 0x02; //
-        console.log('But we are sending: ', showInstead);
-        data[bufferIndex] = showInstead;
+        //console.log('bitmap: ', data.slice(29,34));
+        //console.log('Buffer Index: ', bufferIndex);
+        console.log('Server Says the mainScreen is: ', getPrettyName(data.readUInt8(bufferIndex)));
+        data.writeUInt8(showInstead, bufferIndex);
+        console.log('But we are sending: ', getPrettyName(data.readUInt8(bufferIndex)));
       };
     };
 
@@ -114,72 +117,74 @@ var server = net.createServer(function(listen) {
     if (program.packet) console.log(chalk.blue(data));
   });
   listen.on('end', function() {
-    // if (program.verbose) console.log(new Date() + ' (listening) socket end.');
     logSocketStatus(' (listening) end ' , listen);
     listen.forward.end();
     listen.end();
   });
   listen.on('error', function(err) {
-    // if (program.verbose) console.log(new Date() + ' (listening) error: ' + err);
     logSocketStatus(' (listening) error ' , listen);
     listen.forward.destroy();
   });
   listen.on('close', function() {
-    // var args = Array.prototype.slice.call(arguments);
     logSocketStatus(' (listening) close ' , listen);
-    // if (program.verbose) console.log(new Date() + ' (listening) close: ' + args);
     listen.forward.end();
   });
 });
 
 server.listen(program.listen ? program.listen : program.listenuds, function() {
-  console.log('tcpslow ' + packagejson.version);
+  console.log('Mainscreen View Manager version' + packagejson.version);
   if (program.listen) {
     console.log('Listening on port ' + program.listen);
-  } else {
-    console.log('Listening on unix domain socket ' + program.listenuds);
   }
-  console.log('Relaying to ' + (program.host ? program.host + ' ' : '')
-    + 'port ' + program.forward);
-  if (program.delay) {
-    console.log(' delaying by ' + program.delay + 'ms' + (program.sending ? ' in both directions ' : ' on receive'));
-  }
-});
+  console.log('Relaying to ' + (program.host ? program.host + ' ' : '') + 'port ' + program.forward);
+  showInstead = program.view;
+  console.log('View will be set to: ', getPrettyName(showInstead));
+  console.log('Use Control+C to quit.');
+  });
 
 function unpackBitmap(bufferSlice, buffer) { //Returns the number of bytes to skip.
-//console.log('In unpack.');
+
 //                0 1 2 3 4 5 6 7 8 9 10111213141516171819202122232425262728293031
-var fieldBytes = [4,4,4,4,4,1,1,4,2,4,4,4,4,4,4,4,4,4,2,0,4,4,4,4,4,1,4,1,0,0,0,0];
-var bitMapped = Array(32);
-var bitmap = Array(4);
+var fieldBytes = [4,4,4,4,4,1,1,4,2,4,4,4,4,4,4,4,4,4,2,0,4,4,4,4,4,1,4,0,0,0,0,0]; //As of Artemis 2.1.5
+var bitMapped = Array(40);
+var bitmap = Array(5);
 var runTot = 34; //Running Total of bytes we can skip.
 var i = 0;
 var byte = 0;
-vay bit = 0;
+var bit = 0;
 
   for (i = 0; i < 4 ; i++) {
     bitmap[i] = bufferSlice.readUInt8(i);
   };
   for (byte = 0; byte < 4; byte++) {
     for (bit = 0; bit < 8; bit++) {
-      bitMapped[(byte*8)+bit] = (bitmap[byte] >> (7-bit) & 1);
-    };//End For Bit
-  };//End For Byte
+      bitMapped[(byte*8)+bit] = (bitmap[byte] >> (0+bit) & 1);
+    }; //End For Bit
+  }; //End For Byte
 
-  if (!bitMapped[28]) { //If this is false we don't have to care anymore.
-    //console.log('Early Exit: ', runTot);
-    return 0;
-  }
+  if (!bitMapped[27]) return 0; //We can early exit if the update does not include the Mainscreen view.
 
-  for (i=0;i<32;i++){
-      if (bitMapped[i]) runTot += fieldBytes[i];
-      if (i === 19 && bitMapped[i]) {
+  for (i=0;i<28;i++){ //Only need to look until we get to the item we want.
+      if (i === 19 && bitMapped[i] === 1) {
         var strLen = (buffer.readUInt32LE(runTot)*2);
         var str = buffer.toString('utf16le', runTot + 4, runTot + strLen + 2);
-        runTot += strLen + 4;
+        console.log('str: ', str);
+        fieldBytes[i] = strLen + 4;
+      };
+      if (bitMapped[i]) {
+        runTot += fieldBytes[i];
       };
   };
-
   return runTot;
+}; //End Function
 
+function getPrettyName(theView) {
+  if (theView == 0) return 'Fore';
+  if (theView == 1) return 'Port';
+  if (theView == 2) return 'Starboard';
+  if (theView == 3) return 'Aft';
+  if (theView == 4) return 'Tactical';
+  if (theView == 5) return 'LRS';
+  if (theView == 6) return 'Status';
+  return 'Out of Bounds';
 }; //End Function
